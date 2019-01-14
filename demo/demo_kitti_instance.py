1315079615e36692948a31290450f32ee4d5b947
u87249350 <87249350@tianchi.com>
2019-01-14 03:58:20 -0500

    modify kitti instance names in visualisation

--- demo/demo_kitti_instance.py
+++ demo/demo_kitti_instance.py
@@ -16,13 +16,30 @@ config_file = "../configs/e2e_mask_rcnn_R_101_FPN_1x_kitti_instance.yaml"
 cfg.merge_from_file(config_file)
 # manual override some options
 cfg.merge_from_list(["MODEL.DEVICE", "cpu"])
-cfg.merge_from_list(["MODEL.WEIGHT", "/media/HDD_4TB/MSCOCO/experiments/e2e_mask_rcnn_R_101_FPN_1x_kitti_instance/Jan10-23-02_n606_step/model_0002500.pth"])
+cfg.merge_from_list(["MODEL.WEIGHT", "/media/SSD_1TB/Kitti/experiments/e2e_mask_rcnn_R_101_FPN_1x_kitti_instance/Jan10-23-02_n606_step/model_0002500.pth"])
 
 coco_demo = COCODemo(
     cfg,
     min_image_size=800,
     confidence_threshold=0.7,
+    show_mask_heatmaps=False,
 )
+
+category_id_to_contiguous_id = {
+              'background': 0,
+              'bicycle': 1,
+              'bus': 2,
+              'car': 3,
+              'caravan': 4,
+              'motorcycle': 5,
+              'person': 6,
+              'rider': 7,
+              'trailer': 8,
+              'train': 9,
+              'truck': 10}
+
+coco_demo.CATEGORIES = list(category_id_to_contiguous_id.keys())
+
 # load image and then run prediction
 img_dir = "/media/SSD_1TB/Kitti/data_semantics/testing/image_2"
 img_files = os.listdir(img_dir)
