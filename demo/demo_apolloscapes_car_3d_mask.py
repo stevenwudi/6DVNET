5ebac52ea72c16fadbd62080c003de7fbf804f6c
u87249350 <87249350@tianchi.com>
2019-01-27 21:42:25 -0500

    Apollo Car 3d instance test demo added

---
+++ demo/demo_apolloscapes_car_3d_mask.py
@@ -0,0 +1,40 @@
+from maskrcnn_benchmark.config import cfg
+from predictor import KittiDemo
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+import os
+
+
+def imshow(img):
+    plt.imshow(img[:, :, [2, 1, 0]])
+    plt.axis("off")
+
+config_file = "../configs/e2e_3d_car_101_FPN_triple_head.yaml"
+
+# update the config options with the config file
+cfg.merge_from_file(config_file)
+# manual override some options
+#cfg.merge_from_list(["MODEL.DEVICE", "cpu"])
+cfg.merge_from_list(["MODEL.WEIGHT", "/media/SSD_1TB/ApolloScape/6DVNET_experiments/e2e_3d_car_101_FPN_triple_head/Jan24-03-56_n606_step/model_0010000.pth"])
+
+coco_demo = KittiDemo(
+    cfg,
+    min_image_size=800,
+    confidence_threshold=0.7,
+    show_mask_heatmaps=False,
+)
+
+category_id_to_contiguous_id = {'background': 0, 'car': 1}
+
+coco_demo.CATEGORIES = list(category_id_to_contiguous_id.keys())
+
+# load image and then run prediction
+img_dir = "/media/SSD_1TB/ApolloScape/ECCV2018_apollo/test/images"
+img_files = sorted(os.listdir(img_dir))
+img = Image.open(os.path.join(img_dir, img_files[0])).convert("RGB")
+image = np.array(img)[:, :, [2, 1, 0]]
+
+predictions = coco_demo.run_on_opencv_image(image)
+
+imshow(predictions)
